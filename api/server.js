57ef08771e0f8b29c5515f8914157b67150e98b9
francolivelli <franco.livelli@gmail.com>
2022-11-02 19:39:58 -0300

    again

---
+++ api/server.js
@@ -0,0 +1,18 @@
+// Configuración del server
+
+const express = require("express");
+const app = express();
+const volleyball = require("volleyball");
+const db = require("./config/db");
+const models = require("./models")
+
+app.use(express.json());
+app.use(volleyball);
+
+db.sync({ force: false })
+  .then(() => {
+    app.listen(3001, () => {
+      console.log(`Servidor corriendo en el puerto 3001`);
+    });
+  })
+  .catch(console.error);
