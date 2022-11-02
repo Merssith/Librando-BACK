d5ebf519f57d8f9bf89b50d36eef09e4758f338f
Merssith <mercedes.salcedo1989@gmail.com>
2022-11-02 17:59:07 -0300

    ADD: Orders routes/controller/service

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const products = require("./product.js");
+const orders = require("./order.js");
 
 router.use("/product", products);
+router.use("/order", products);
 
 module.exports = router;
