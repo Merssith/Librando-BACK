9b3deb1f125ad973c8e46e2c9da222e57931d2ea
miklzn <mikelzonsarabia@gmail.com>
2022-11-03 16:38:08 -0300

    Primeras rutas Users

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const products = require("./product.js");
 const orders = require("./order.js");
+const users = require("./users");
 
+router.use("/users", users);
 router.use("/product", products);
-router.use("/order", products);
+router.use("/order", orders);
 
 module.exports = router;
