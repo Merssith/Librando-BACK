0a22a2c21dc052499de01e873c4b15860b4f4fa3
Merssith <mercedes.salcedo1989@gmail.com>
2022-11-08 11:47:20 -0300

    ADD: New route: list books by genre

--- api/services/genreService.js
+++ api/services/genreService.js
@@ -1,4 +1,6 @@
-const { Genre } = require("../models");
+const { Genre, Book, Review } = require("../models");
+const sequelize = require("sequelize");
+const Op = sequelize.Op;
 
 exports.findAll = () => {
   return Genre.findAll({ attributes: { exclude: ["bookId"] } });
@@ -6,6 +8,32 @@ exports.findAll = () => {
 
 exports.findById = (id) => {
   return Genre.findByPk(id, { attributes: { exclude: ["bookId"] } });
+};
+
+exports.findBooksByGenreIdOrName = async (queryString) => {
+  if (!isNumeric(queryString)) {
+    let searchId = await Genre.findOne({
+      where: {
+        name: {
+          [Op.iLike]: queryString,
+        },
+      },
+    });
+    queryString = searchId.dataValues.id;
+  }
+  let books = await Book.findAll({
+    where: { genreId: queryString },
+    include: [
+      {
+        model: Review,
+        required: false,
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+      },
+    ],
+    attributes: { exclude: ["bookOrderId"] },
+  });
+  await getGenre(books);
+  return books;
 };
 
 exports.create = (genre) => {
@@ -21,3 +49,20 @@ exports.change = async (id, changes) => {
 exports.delete = (id) => {
   return Genre.destroy({ where: { id } });
 };
+
+// ADITIONAL FUNCTIONS
+
+function isNumeric(str) {
+  if (typeof str != "string") return false;
+  return !isNaN(str) && !isNaN(parseFloat(str));
+}
+
+async function getGenre(bookArray) {
+  for (i = 0; i < bookArray.length; i++) {
+    let genreId = bookArray[i].genreId;
+    let genre = await Genre.findByPk(genreId, {
+      attributes: { exclude: ["createdAt", "updatedAt", "bookId", "id"] },
+    });
+    bookArray[i].dataValues.genre = genre.dataValues;
+  }
+}
